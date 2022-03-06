9cd19e47a72c6f884c50ae5751a52aec4a717628
FeGaWeb <andresgalviscamacho21@gmail.com>
2022-03-05 22:32:55 -0500

    version 2

---
+++ js/main.js
@@ -0,0 +1,4 @@
+const addToShopeeCardButton = document.querySelectorAll('.addToCart');
+addToShopeeCardButton.forEach(addToCartButton => {
+    addToCartButton.addEventListener('click', () => console.log('click'))
+})
